6b548562617175e09296b0301fd8eea4276006c0
amitramesh <amit.ramesh.sharma@gmail.com>
2020-04-20 18:51:37 +0530

    Add Razor Page

---
+++ ToDoOdt/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace ToDoOdt.Pages.Restaurants
+{
+    public class List : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public List(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string MessageData { get; set; }
+        
+        public void OnGet()
+        {
+            MessageData = _configuration["MessageData"];
+        }
+    }
+}
