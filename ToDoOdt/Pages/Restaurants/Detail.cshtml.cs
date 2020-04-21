074d173f294e2ed461b404fbc64c39cc2f62d9a3
amitramesh <amit.ramesh.sharma@gmail.com>
2020-04-21 22:23:04 +0530

    Detail Page Implementation

---
+++ ToDoOdt/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoOdt.Core;
+using ToDoOdt.Data;
+
+namespace ToDoOdt.Pages.Restaurants
+{
+    public class Detail : PageModel
+    {
+        private readonly IRestaurantData _restaurantData;
+        public Restaurant Restaurant { get; private set; }
+        public Detail(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+        public void OnGet(int restaurantId)
+        {
+            Restaurant = _restaurantData.GetRestaurantById(restaurantId);
+        }
+    }
+}
