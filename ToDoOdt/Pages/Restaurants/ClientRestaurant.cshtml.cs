017e0597b9b604aab6c89e07439d96c1ef9734ef
amitramesh <amit.ramesh.sharma@gmail.com>
2020-04-23 21:36:18 +0530

    Create a API controller and call consume by java script client.

---
+++ ToDoOdt/Pages/Restaurants/ClientRestaurant.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoOdt.Pages.Restaurants
+{
+    public class ClientRestaurant : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
